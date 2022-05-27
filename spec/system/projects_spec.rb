a60797668952963f6fc9793b2bc0bc71bd9b2eae
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-27 10:24:51 +0900

    演習問題8

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
+  let(:type_test_project){
+    fill_in "Name", with: "Test Project"
+    fill_in "Description", with: "Trying out Capybara"
+  }
+
   scenario "user creates a new project" do
     user = FactoryBot.create(:user)
     # using our custom login helper:
@@ -12,8 +17,7 @@ RSpec.describe "Projects", type: :system do
 
     expect {
       click_link "New Project"
-      fill_in "Name", with: "Test Project"
-      fill_in "Description", with: "Trying out Capybara"
+      type_test_project
       click_button "Create Project"
 
       aggregate_failures do
@@ -22,5 +26,29 @@ RSpec.describe "Projects", type: :system do
         expect(page).to have_content "Owner: #{user.name}"
       end
     }.to change(user.projects, :count).by(1)
+  end
+
+  scenario "ユーザーがプロジェクトを編集する" do
+    user = FactoryBot.create(:user)
+    project = FactoryBot.create(:project ,owner: user)
+    # using our custom login helper:
+    # sign_in_as user
+    # or the one provided by Devise:
+    sign_in user
+
+    visit root_path # refactor: 直接`project`へアクセスしたいが project_pathではアクセスできなかった。
+    click_link "Project 1" # refactor: 同上
+
+    expect {
+      click_link "Edit"
+      type_test_project
+      click_button "Update Project"
+
+      aggregate_failures do
+        expect(page).to have_content "Project was successfully updated"
+        expect(page).to have_content "Test Project"
+        expect(page).to have_content "Owner: #{user.name}"
+      end
+    }.to change(user.projects, :count).by(0)
   end
 end
