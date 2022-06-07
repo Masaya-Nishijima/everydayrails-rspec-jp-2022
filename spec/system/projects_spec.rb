5ee5e090f92f52a7381e74c063ef8e95b406244f
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-07 10:04:49 +0900

    refactor: スペックを木構造化し、beforeを利用

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,77 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
-  scenario "user creates a new project" do
-    user = FactoryBot.create(:user)
-    # using our custom login helper:
-    # sign_in_as user
-    # or the one provided by Devise:
-    sign_in user
+  context "プロジェクト作成" do
+    scenario "user creates a new project" do
+      user = FactoryBot.create(:user)
+      # using our custom login helper:
+      # sign_in_as user
+      # or the one provided by Devise:
+      sign_in user
 
-    visit root_path
+      visit root_path
 
-    expect {
-      click_link "New Project"
-      fill_in "Name", with: "Test Project"
-      fill_in "Description", with: "Trying out Capybara"
-      click_button "Create Project"
+      expect {
+        click_link "New Project"
+        fill_in "Name", with: "Test Project"
+        fill_in "Description", with: "Trying out Capybara"
+        click_button "Create Project"
 
-      aggregate_failures do
-        expect(page).to have_content "Project was successfully created"
-        expect(page).to have_content "Test Project"
-        expect(page).to have_content "Owner: #{user.name}"
-      end
-    }.to change(user.projects, :count).by(1)
+        aggregate_failures do
+          expect(page).to have_content "Project was successfully created"
+          expect(page).to have_content "Test Project"
+          expect(page).to have_content "Owner: #{user.name}"
+        end
+      }.to change(user.projects, :count).by(1)
+    end
   end
 
-  scenario "user completes a project" do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, owner: user)
-    sign_in user
+  context "コンプリート機能の動作" do
+    before do
+      @user = FactoryBot.create(:user)
+      @project = FactoryBot.create(:project, owner: @user)
+      sign_in @user
+    end
 
-    visit project_path(project)
+    scenario "user completes a project" do
+      visit project_path(@project)
 
-    expect(page).to_not have_content "Completed"
+      expect(page).to_not have_content "Completed"
 
-    click_button "Complete"
+      click_button "Complete"
 
-    expect(project.reload.completed?).to be true
-    expect(page).to \
-      have_content "Congratulations, this project is complete!"
-    expect(page).to have_content "Completed"
-    expect(page).to_not have_button "Complete"
-  end
+      expect(@project.reload.completed?).to be true
+      expect(page).to \
+        have_content "Congratulations, this project is complete!"
+      expect(page).to have_content "Completed"
+      expect(page).to_not have_button "Complete"
+    end
 
-  scenario "コンプリート済みのプロジェクトを非表示にする" do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, owner: user)
-    sign_in user
+    scenario "コンプリート済みのプロジェクトを非表示にする" do
+      visit root_path
+      expect(page).to have_content @project.name
 
-    visit root_path
-    expect(page).to have_content project.name
+      visit project_path(@project)
 
-    visit project_path(project)
+      click_button "Complete"
 
-    click_button "Complete"
+      visit root_path
+      expect(page).to_not have_content @project.name
+      #一覧画面でリンクに打ち消し線を付ける。
+    end
 
-    visit root_path
-    expect(page).to_not have_content project.name
-    #一覧画面でリンクに打ち消し線を付ける。
-  end
+    scenario "完了済みもShowAllボタンを押されたら表示する" do
+      visit project_path(@project)
 
+      click_button "Complete"
 
-  scenario "完了済みもShowAllボタンを押されたらコンプリート済みも表示する" do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, owner: user)
-    sign_in user
+      visit root_path
 
-    visit project_path(project)
-
-    click_button "Complete"
-
-    visit root_path
-
-    click_link "ShowAll"
-    expect(page).to have_content project.name
+      click_link "ShowAll"
+      expect(page).to have_content @project.name
+    end
   end
 end
