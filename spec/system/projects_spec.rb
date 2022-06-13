916f230295524575035c773c9c405ac406ec2cd9
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-13 15:14:42 +0900

    refactor: beforeをより利用し、DRYに

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Projects", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    sign_in @user
+  end
   context "プロジェクト作成" do
     scenario "user creates a new project" do
-      user = FactoryBot.create(:user)
-      # using our custom login helper:
-      # sign_in_as user
-      # or the one provided by Devise:
-      sign_in user
-
       visit root_path
 
       expect {
@@ -20,17 +18,15 @@ RSpec.describe "Projects", type: :system do
         aggregate_failures do
           expect(page).to have_content "Project was successfully created"
           expect(page).to have_content "Test Project"
-          expect(page).to have_content "Owner: #{user.name}"
+          expect(page).to have_content "Owner: #{@user.name}"
         end
-      }.to change(user.projects, :count).by(1)
+      }.to change(@user.projects, :count).by(1)
     end
   end
 
   context "コンプリート機能の動作" do
     before do
-      @user = FactoryBot.create(:user)
       @project = FactoryBot.create(:project, owner: @user)
-      sign_in @user
     end
 
     scenario "user completes a project" do
