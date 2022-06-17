c8e167b6d99ba27e70cc300fb2f1e74beb249b81
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-17 09:11:08 +0900

    feat: redirect_to マッチャの利用

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe "Home page", type: :request do
     before { get root_path }
     it { expect(response).to have_http_status(:success) }
   end
+
+  context "ログインを要求するリダイレクト" do
+    before {get projects_path	}
+    it { expect(response).to redirect_to(user_session_path) }
+    # http://localhost:3000/projects
+    # http://localhost:3000/users/sign_in
+  end
 end
